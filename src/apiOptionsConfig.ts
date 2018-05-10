77bedc08a215cf7242ba07d644ad356c9bb41712
Zloteanu Anastasia <pukapuka@192-168-0-101.rdsnet.ro>
2018-05-10 19:15:55 +0300

    Implement feedback

---
+++ src/apiOptionsConfig.ts
@@ -0,0 +1,29 @@
+import { STREAMLABS_CLIENT_ID, STREAMLABS_CLIENT_SECRET, TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET } from './config'
+
+export const twitchToken = {
+  client_id: TWITCH_CLIENT_ID,
+  client_secret: TWITCH_CLIENT_SECRET,
+  grant_type: 'authorization_code',
+  redirect_uri: 'http://localhost:8000/api/v1/auth/twitch/callback',
+}
+
+export const streamlabsToken = {
+  grant_type: 'authorization_code',
+  client_id: STREAMLABS_CLIENT_ID,
+  client_secret: STREAMLABS_CLIENT_SECRET,
+  redirect_uri: 'http://localhost:8000/api/v1/auth/streamlabs/callback',
+}
+
+export const twitchStrategyOptions = {
+  clientID: TWITCH_CLIENT_ID,
+  clientSecret: TWITCH_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8000/api/v1/auth/twitch/callback',
+  scope: 'user_read',
+}
+
+export const streamlabsStrategyOptions = {
+  clientID: STREAMLABS_CLIENT_ID,
+  clientSecret: STREAMLABS_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8000/api/v1/auth/streamlabs/callback',
+  scope: 'alerts.create',
+}
--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { stringify } from 'qs'
-import { Controller, Get, QueryParams, Request, Res } from 'ts-express-decorators'
-import { STREAMLABS_CLIENT_ID, STREAMLABS_CLIENT_SECRET, TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET } from '../config'
+import { Controller, Get, QueryParams, Request, Res, Response } from 'ts-express-decorators'
+import { streamlabsToken, twitchToken } from '../apiOptionsConfig'
+import { ENV } from '../config'
 import { passportInstance } from '../services/PassportService'
 
 @Controller('/auth')
@@ -20,22 +21,23 @@ export class AuthController {
   @Get('/twitch/callback')
   public callbackTwitch(
     @QueryParams('code') code: string,
+    @Response() response: any,
   ) {
-    const data = stringify({client_id: TWITCH_CLIENT_ID,
-      client_secret: TWITCH_CLIENT_SECRET,
+    const data = stringify({
+      ...twitchToken,
       code,
-      grant_type: 'authorization_code',
-      redirect_uri: 'http://localhost:8000/api/v1/auth/twitch/callback',
     })
 
-    return axios.post(`https://id.twitch.tv/oauth2/token?client_id=${TWITCH_CLIENT_ID}`, data).then((res) => {
-      return res.data
+    const baseUrl = ENV === 'production' ? 'https://cryptopotam.us' : 'http://localhost:3000'
+
+    return axios.post(`https://id.twitch.tv/oauth2/token`, data).then((res) => {
+      response.redirect(`${baseUrl}/setup?access_token=${res.data.access_token}`)
     }).catch((err) => {
       return err.response.data
     })
   }
 
-@Get('/streamlabs')
+  @Get('/streamlabs')
   public loginStreamlabs(
     @Request() request: Express.Request,
     @Res() response: Express.Response,
@@ -45,15 +47,12 @@ export class AuthController {
     })
   }
 
-@Get('/streamlabs/callback')
+  @Get('/streamlabs/callback')
   public callbackStreamlabs(
-@QueryParams('code') code: string,
+    @QueryParams('code') code: string,
   ) {
     return axios.post(`https://streamlabs.com/api/v1.0/token`, {
-      grant_type: 'authorization_code',
-      client_id: STREAMLABS_CLIENT_ID,
-      client_secret: STREAMLABS_CLIENT_SECRET,
-      redirect_uri: 'http://localhost:8000/api/v1/auth/streamlabs/callback',
+      ...streamlabsToken,
       code,
     })
       .then((res) => {
