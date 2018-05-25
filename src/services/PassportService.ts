a6eda0b119065532e237744f2d2faed9d84b1d2f
Dennis Li <dennis.li.92@gmail.com>
2018-05-25 14:44:30 -0700

    more error handling

--- src/services/PassportService.ts
+++ src/services/PassportService.ts
@@ -22,33 +22,38 @@ export class PassportService implements BeforeRoutesInit {
     this.expressApplication.use(passport.initialize({ userProperty }))
     this.expressApplication.use(passport.session({ pauseStream }))
 
-    passport.use(new twitchStrategy({
-        ...twitchStrategyOptions,
-      },
+    passport.use(new twitchStrategy(
+      { ...twitchStrategyOptions },
       async (accessToken, refreshToken, profile, done) => {
-        let user = await users.findUser(profile.id)
+        try {
+          let user = await users.findUser(profile.id)
 
-        if (!user) {
-          user = await users.addNewUser(profile.id)
-        }
+          if (!user) {
+            user = await users.addNewUser(profile.id)
+          }
 
-        done(null, { user, refreshToken, accessToken })
-      }),
-    )
-
-    passport.use(new streamlabsStrategy({
-      ...streamlabsStrategyOptions,
-    },
-      async (accessToken, refreshToken, profile, done) => {
-        const user = await users.findUser(profile._json.twitch.id)
-        if (!user) {
-          done('No twitch ID/ user not found!', {})
-        } else {
           done(null, { user, refreshToken, accessToken })
+        } catch (err) {
+          done(err)
         }
-        },
-      ),
-    )
+      },
+    ))
+
+    passport.use(new streamlabsStrategy(
+      { ...streamlabsStrategyOptions },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const user = await users.findUser(profile._json.twitch.id)
+          if (!user) {
+            done('No twitch ID/ user not found!', {})
+          } else {
+            done(null, { user, refreshToken, accessToken })
+          }
+        } catch (err) {
+          done (err)
+        }
+      },
+    ))
   }
 
 }
